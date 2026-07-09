e3bfbe19f84e15c210177ec95bec851fd9b81c1b
Ritaabugu <ezeonyinye184@gmail.com>
2026-07-09 12:07:18 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/webp"],
   },
-  async rewrites() {
-    return [{ source: "/admin", destination: "/admin/index.html" }];
+  async redirects() {
+    return [
+      { source: "/admin", destination: "/admin/index.html", permanent: false },
+    ];
   },
 };
 
